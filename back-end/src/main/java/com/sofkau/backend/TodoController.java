1c0e36f3edb390d1c291ca571896bec85dbf36de
Jhon Serna <jhonalfaber@gmail.com>
2021-09-15 11:58:05 -0500

    Se agrega Cross origin en TodoController

--- back-end/src/main/java/com/sofkau/backend/TodoController.java
+++ back-end/src/main/java/com/sofkau/backend/TodoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path = "/api")
 public class TodoController {
     @Autowired
