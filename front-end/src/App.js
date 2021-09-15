394065feb1cdca356836e32cc79f995eae19864f
Jhon Serna <jhonalfaber@gmail.com>
2021-09-15 13:44:41 -0500

    Formulario y funcion push implementados

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { useContext, useReducer, useRef, useState, createContext } from 'react';
+
+const HOST_API = "http://localhost:8080/api";
+const initialState = {
+  todo: { list: [], item: {} }
+};
+const Store = createContext(initialState)
+
+
+const Form = () => {
+  const formRef = useRef(null);
+  const { dispatch, state: { todo } } = useContext(Store);
+  const item = todo.item;
+  const [state, setState] = useState(item);
+  const onAdd = (event) => {
+    event.preventDefault();
+    const request = {
+      name: state.name,
+      id: null,
+      completed: false
+    };
+    fetch(HOST_API + "/todo", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "add-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  }
+
+  return <form ref={formRef}>
+    <input
+      type="text"
+      name="name"
+      placeholder="¿Qué piensas hacer hoy?"
+      defaultValue={item.name}
+      onChange={(event) => {
+        setState({ ...state, name: event.target.value })
+      }}  ></input>
+    {!item.id && <button onClick={onAdd}>Crear</button>}
+  </form>
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'add-item':
+      const todoUp = state.todo.list;
+      todoUp.push(action.item);
+      return { ...state, todo: {list: todoUp, item: {}} }
+    default:
+      return state;
+  }
+}
+
+const StoreProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <Store.Provider value={{ state, dispatch }}>
+    {children}
+  </Store.Provider>
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      Todo bien
-    </div>
-  );
+  return <StoreProvider>
+    <h3>To-Do List</h3>
+    <Form />
+  </StoreProvider>
 }
 
 export default App;
